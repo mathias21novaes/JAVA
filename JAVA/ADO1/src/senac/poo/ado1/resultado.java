2145c017c201d5758c8388e9583ee209732b4bd8
Mathias Novaes <mathias21novaes@gmail.com>
2018-02-27 19:10:42 -0300

    ados

---
+++ JAVA/ADO1/src/senac/poo/ado1/resultado.java
@@ -0,0 +1,12 @@
+package senac.poo.ado1;
+
+public class resultado {
+    
+    public static void main(String[] args) {
+		
+	Logaritmo ln = new Logaritmo();
+	System.out.printf("O valor de Ln de 1.3 eh: %1.10f ", ln.calculo(1.3));
+
+    }
+    
+}
